300c68f38a0b5b66b201f4071bce49bfdeb19b49
Camille Moly <camille.moly@gmail.com>
2021-01-20 14:49:24 +0100

    fix: modify mongoose link (+ comments)

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,31 @@
-const express = require("express");
+const express = require("express"); 
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const app = express();
+const app = express(); // create application express
 const saucesRoutes = require("./routes/sauces");
 const userRoutes = require("./routes/user");
-const path = require('path');
+const path = require("path");
 
-mongoose.connect("mongodb+srv://camille:mdpexmongoDB0801@cluster0.tydhc.mongodb.net/cluster0?retryWrites=true&w=majority",
+// connect app to database
+mongoose.connect("mongodb+srv://camille:KYDO3Ielg9bCYiP2@sopekocko.wmof7.mongodb.net/SoPekocko?retryWrites=true&w=majority",
   { useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+// General middleware => add headers on response object : access origin to everybody, give authorization to use some headers
+// and allow specifics methods
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
   });
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // transform request body in JSON
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use("/api/sauces", saucesRoutes);
+app.use("/images", express.static(path.join(__dirname, "images"))); // for requests to "/images", serves the static folder "images"
+app.use("/api/sauces", saucesRoutes); // save router for all requests made to "/api/sauces"
 app.use("/api/auth", userRoutes);
 
-module.exports = app;
+module.exports = app; // export app to access it from others files
